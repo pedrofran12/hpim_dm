2e5fd4c506931f866acf3ff1809267ac60696179
Pedro Oliveira <pedrofran.oliveira@hotmail.com>
2020-05-22 18:33:44 +0100

    update setup.py => requires at least Python v3.3 (drop support to Pythonv3.2)

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
@@ -46,5 +45,5 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    python_requires=">=3.2",
+    python_requires=">=3.3",
 )
